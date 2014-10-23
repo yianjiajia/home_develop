d936e507804234ae5fa2a3ede6e34b2fc0911e2f
Gaga Yan <jiajia.yan@tekview.com>
2014-10-23 18:46:16 +0800

    修改逻辑”

--- autodeploy.py
+++ autodeploy.py
@@ -5,35 +5,41 @@
 import sys, os
 import subprocess
 import re
+import shlex
 filename=sys.argv[0]
-DICT= {"192.168.0.30":"/home/apache-tomcat-7.0.55",
-                  "192.168.0.35":"/root/apache-tomcat-7.0.55",
-                  "192.168.0.39":"/root/apache-tomIcat-7.0.55"
+DICT= {'192.168.0.30':'/home/apache-tomcat-7.0.55',
+                  '192.168.0.35':'/root/apache-tomcat-7.0.55',
+                  '192.168.0.39":"/root/apache-tomIcat-7.0.55'
                  }
 #执行远程拷贝
 
-def copytosever(*args):   #形式参数file为需要传递的文件；形式参数server为远端服务器IP；形式参数dir为tomcat服务器的目录
+def copytosever():   #形式参数file为需要传递的文件；形式参数server为远端服务器IP；形式参数dir为tomcat服务器的目录
 	print '#*20\n'
-	print '拷贝%s到%s的%s' %(args[0],args[1],args[2])
+	line = raw_input("依次输入拷贝文件、server的IP，保存目录，名称以空格分隔:\n").split()
+	command_line = ['ssh',line[0],'root@'+line[1]+':'+line[2]]
+	args = shlex.split(command_line)
 	try:
-		P=subprocess.Popen('scp args[0] root@args[1]:args[2]')
+		P = subprocess.Popen(args)
 		p.wait()
 	except IOError, e:
 		print e
 def changeDB(*args):
 	print '#*20\n'
-	print '修改数据库' 
-	cmd=['mysql -uroot -p111111 < sqlfile' for sqlfile in args] 
+	sql =  raw_input("请按sql脚本的执行顺序依次输入，文件以空格分隔:\n").split()
+	cmd = ['mysql -uroot -p111111 < sqlfile' for sqlfile in args] 
+	args = shlex.split(cmd)
 	try:
 		for x in cmd:
-			P=subprocess.Popen("ssh -p root@192.168.0.35 x")
+			P = subprocess.Popen(args)
 			p.wait()
 	except IOError, e:
 		print e
 def manger_tomcat():
-	opt=raw_input("请输入远端服务器ip及脚本名,以空格分开\n").split()
+	opt = raw_input("请输入远端服务器ip及脚本名,以空格分开\n").split()
+	command_line = ['ssh','root@'+line[0],DICT[line[0]]+'/'+line[1]]
+	args = shlex.split(command_line)
 	try:
-		P=subprocess.Popen("ssh -p root@ops[0]:DICT[ops[0]]/bin/opt[1]")
+		P = subprocess.Popen(args)
 		p.wait()
 	except IOError, e:
 		print e
@@ -73,11 +79,9 @@ if __name__ == '__main__':
        		sys.exit()
 	ope = sys.argv[1]
 	if ope == 'copytoserver':
-		a = raw_input("依次输入拷贝文件、server的IP，保存目录，名称以空格分隔:\n").split()
-		copytosever(a)
+		copytosever()
 	elif ope == 'changeDB':
-		b =  raw_input("请按sql脚本的执行顺序依次输入，文件以空格分隔:\n").split()
-		changeDB(b)
+		changeDB()
 	elif ope == 'manger_tomcat':
 		manger_tomcat()
 	else:
