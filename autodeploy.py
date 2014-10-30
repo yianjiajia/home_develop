387850e29f4a30c858495ac438476944e0b03163
gaga.yan <yianjiajia@126.com>
2014-10-30 13:01:14 +0800

    add new autodeploy.py

--- autodeploy.py
+++ autodeploy.py
@@ -1,92 +1,84 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
-
-
+# -*- coding: utf-8 -*-
 import sys, os
 import subprocess
 import re
 import shlex
-filename=sys.argv[0]
-DICT= {'192.168.0.30':'/home/apache-tomcat-7.0.55',
-                  '192.168.0.35':'/root/apache-tomcat-7.0.55',
-                  '192.168.0.39':'/root/apache-tomIcat-7.0.55'
-      }
-#copytoserver执行远程拷贝
+import time
+filename = sys.argv[0]
+ABSDIR = os.path.abspath(os.path.dirname(__file__))
+DICT = {'192.168.0.30': '/home/apache-tomcat-7.0.55',
+        '192.168.0.35': '/root/apache-tomcat-7.0.55',
+        '192.168.0.39': '/root/apache-tomcat-7.0.55'
+        }
+# copytoserver执行远程拷贝,将sql文件拷贝至35的/root/test目录并执行、war文件拷贝至DICT字典所指定的目录，
+def copytoserver():
+    filelist = [i for i in os.listdir(ABSDIR) if i.endswith('sql') or i.endswith('war')]
+    try:
+        for s in filelist:
+            if s.endswith('war'):
+                for k, v in DICT.items():
+                    print "\n\n拷贝%s到%s的%s/webapps/目录......"%(s,k,v)
+		    command_line1 = ['ssh', 'root@'+k, 'rm -rf ', v+'/webapps'+'/bankriskcontrol' ]
+                    command_line2 = ['scp', s, 'root@'+k+':'+v]
+                    p1 = subprocess.Popen(command_line1)
+                    p1.wait()
+                    p2 = subprocess.Popen(command_line2)
+                    p2.wait()
+		    print "\n\n"
+            else:
+                print "\n\n拷贝%s到192.168.0.35的/root/test目录......"%s
+		command_line3 = ['scp', s, 'root@192.168.0.35:/root/test']
+                p3 = subprocess.Popen(command_line3)
+                p3.wait()
+    except IOError, e:
+        print e
 
-def copytosever():   #形式参数file为需要传递的文件；形式参数server为远端服务器IP；形式参数dir为tomcat服务器的目录
-	line = raw_input("依次输入拷贝文件、server的IP，保存目录，名称以空格分隔:\n").split()
-	command_line = ['scp',line[0],'root@'+line[1]+':'+line[2]]
-	try:
-		P = subprocess.Popen(command_line)
-		P.wait()
-	except IOError, e:
-		print e
 
 #changeDB执行sql脚本
-def changeDB():
-	print '#*20\n'
-	sql =  raw_input("请输入你要执行的sql脚本名称:\n").split()
-	cmd = ['ssh','root@'+DICT[line[1]],'mysql -uroot -p111111 <',sql[0]] 
-	try:
-		P = subprocess.Popen(cmd)
-		P.wait()
-			
-	except IOError, e:
-		print e
+def changedb():
+    sql = ['create_database.sql', 'init_database_data.sql', 'addtestdata.sql']
+#    path = '/usr/local/mysql/bin/'
+    try:
+        print "\n\n创建数据库......\n\n"
+	cmd1 = ['ssh', 'root@192.168.0.35', 'mysql', '-uroot', '-p111111', '<', '/root/test/'+sql[0]]
+	p1 = subprocess.Popen(cmd1)
+        p1.wait()
+	print "\n\n初始化数据库......\n\n"
+	cmd2 = ['ssh', 'root@192.168.0.35', 'mysql', '-uroot', '-p111111', '-Dbank_risk_control', '<', '/root/test/'+sql[1]]
+        p2 = subprocess.Popen(cmd2)
+        p2.wait()
+	print "\n\n添加测试数据......\n\n"
+	cmd3 = ['ssh', 'root@192.168.0.35', 'mysql', '-uroot', '-p111111', '-Dbank_risk_control', '<', '/root/test/'+sql[2]]
+        p3 = subprocess.Popen(cmd3)
+        p3.wait()
+	
+    except IOError, e:
+        print e
+
+
 #管理远端tomcat服务器
-def manger_tomcat():
-	line = raw_input("请输入tomcat服务器IP地址,\n").split()
-	command_line = ['ssh','root@'+line[0],DICT[line[0]]+'/bin/'+sys.argv[2]]
-	try:
-		P = subprocess.Popen(command_line)
-		P.wait()
-	except IOError, e:
-		print e
+def restart_tomcat():
+    try:
+        for k, v in DICT.items():
+            command_line4 = ['ssh', 'root@'+k, v+'/bin/shutdown.sh']
+            command_line5 = ['ssh', 'root@'+k, v+'/bin/startup.sh']
+            print "\n\n%s的tomcat服务正在重启.........\n\n"%k
+            p1 = subprocess.Popen(command_line4)
+            p1.wait()
+            p2 = subprocess.Popen(command_line5)
+            p2.wait()
+	    print "\n\n============================================="
+    except IOError, e:
+        print e
 
 if __name__ == '__main__':
-	if len(sys.argv) < 2:
-    		print '请输入运行参数,你可以执行python %s --help查看用法'%filename
-    		sys.exit()
-
-	if sys.argv[1].startswith('--'):
-   		option = sys.argv[1][2:]
-    		if  option == 'version':
-     			print 'Version 1.0'
-   		elif option == 'help':
-     			print '''
-           
-			@author：Gaga.yan 
-			
-			           自动化部署银监会测试环境
-			
-			usage:
-			**********************************************
-			*执行远程拷贝：	
-			* python %s copytoserver  
-			*
-			*修改数据库
-			* python %s changeDB 
-			*
-			*tomcat服务管理
-			* python %s manger_tomcat startup.sh|shutdown.sh
-			*
-			**********************************************
-			'''%(filename,filename,filename)
-   			sys.exit()
-		else:
-       			print 'Unknown option.'
-       		sys.exit()
-	ope = sys.argv[1]
-	if ope == 'copytoserver':
-		copytosever()
-	elif ope == 'changeDB':
-		changeDB()
-	elif ope == 'manger_tomcat':
-		manger_tomcat()
-	else:
-		print '你可以执行python %s --help查看用法'%filename
-
-		
-
-	
-				
+    print '\033[1;31;40m'
+    print '*'*40, '\n'
+    copytoserver()
+    print '\033[1;31;40m'
+    print '*'*40, '\n'
+    changedb()
+    print '\033[1;31;40m'
+    print '*'*40, '\n'
+    restart_tomcat()
